ebd059138aea4e1f09d4a2de7f8880ef7fae1f14
NoahYannis <noah.guderjahn@gmx.de>
2022-05-26 12:46:02 +0200

    Projektdateien hinzufügen.

---
+++ StringReverser/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringReverser
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the string you want to reverse: ");
+            string stringToReverse = Console.ReadLine();
+            Console.WriteLine("---------------------");
+
+            char[] charArray = new char [stringToReverse.Length];
+            char[] charArrayBackwards = new char [stringToReverse.Length];
+
+            StringBuilder s = new StringBuilder();
+
+            for (int i = 0; i < stringToReverse.Length; i++)
+            {
+                charArray[i] = stringToReverse[i];
+                charArrayBackwards[i] = stringToReverse[stringToReverse.Length - 1 - i];
+                s.Append(charArrayBackwards[i]);
+            }
+
+            Console.WriteLine(s);
+            Console.ReadLine();
+        }
+    }
+}
